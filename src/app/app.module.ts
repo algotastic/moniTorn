e3510076643e7ad294cd51597c337e63ea72fbe8
Adam Tjaden <algotastic@gmail.com>
2017-09-24 20:04:46 -0500

    Add test alert using ngx-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlertModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, AppBootstrapModule
+    BrowserModule, AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
